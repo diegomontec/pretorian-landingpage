f7b1e47f1f833563c41b8c4cc0b9169224fd923b
helberkaue <helber.avelino05@gmail.com>
2025-08-13 17:02:07 -0300

    Adicionando o configuração do GTM

--- .next/server/chunks/ssr/[root-of-the-server]__8932d786._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__8932d786._.js
@@ -1047,7 +1047,7 @@ const SectionSix = ()=>{
                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("iframe", {
                     src: "https://b24-2u1rlh.bitrix24.site/crm_form_fxs2v/",
                     title: "Formulário de Contato",
-                    className: "w-full h-[400px] sm:h-[508px] lg:h-[608px] rounded-xl border-none overflow-hidden",
+                    className: "w-full overflow-hidden h-[400px] sm:h-[508px] lg:h-[608px] rounded-xl border-none",
                     style: {
                         backgroundColor: 'transparent'
                     }
